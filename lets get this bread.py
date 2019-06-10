aab8b9989dc8e3d5de4998d3f0fa7808e70ec610
victoriae525 <51681172+victoriae525@users.noreply.github.com>
2019-06-10 16:51:59 -0700

    Add files via upload

---
+++ lets get this bread.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+5/8 #division
+
+print("hello", "5","world")
+print("let's get this bread")
+5*2 #multiplication
+5/2 #division
+5-2 #subtraction
+5**2 #exponent
+(8/9)*3
+def sum(a,b,c):
+    sum = a + b +c
+    print("The sum of", a, b, "and", c, "is", sum)
+sum(2,3,4)
+def areaoftriangle(b,h):
+    areaoftriangle = b * h * 0.5
+    print("area is", areaoftriangle)
+    areaoftriangle(2,3)
